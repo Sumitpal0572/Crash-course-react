86560e1efe3471e7e97ebee3dd4bb8bcf3e95b29
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-09-13 12:07:10 +0530

    Focusable input

---
+++ Day5th/@FocusableInput/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AutoFocusUncontrolledInput from "./components/FocusableInput";
+import ChangeBackground from "./components/ChangeBackground";
+
+function App() {
+  return (
+    <>
+      <AutoFocusUncontrolledInput />
+      <ChangeBackground />
+    </>
+  );
+}
+
+export default App;
