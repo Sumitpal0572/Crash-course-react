9a214d8f7d1a945a0afdff770c9a18847d151eac
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-09-24 11:21:37 +0530

    toggleTheme done

--- Day7th/useReducer/src/App.jsx
+++ Day7th/useReducer/src/App.jsx
@@ -10,9 +10,22 @@ function App() {
   };
 
   return (
-    <div className={"app ${state.theme}"}>
-      <h1>{state.theme.charAt(0).toUpperCase() + state.theme.slice(1)}Mode</h1>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+    <div
+      style={{
+        backgroundColor: state.theme === "light" ? "#fff" : "#333",
+        color: state.theme === "light" ? "#000" : "#fff",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className={"app ${state.theme}"}>
+        <h1>
+          {state.theme.charAt(0).toUpperCase() + state.theme.slice(1)}Mode
+        </h1>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </div>
     </div>
   );
 }
